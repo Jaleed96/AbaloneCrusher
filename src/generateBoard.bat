4065a3430406e8ffe10a497ba6b3d71410737212
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2019-03-28 11:47:51 -0700

    Clean up unneeded files

--- src/generateBoard.bat
+++
@@ -1,6 +0,0 @@
-echo "Compiling java"
-javac TestUtil.java
-echo "Running program"
-rem Please edit the path below to point to the directory that contains all the .input files
-rem The results will be pushed to the same directory
-java TestUtil C:\Users\Jalee\Documents\projects\AbaloneCrusher\src\test\
