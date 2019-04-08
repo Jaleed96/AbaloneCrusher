9b2b2eaf045ccc96a4c7a226afd559751923b336
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2019-04-07 18:54:58 -0700

    Best heuristic so far

--- src/CenterHeuristic.java
+++ src/CenterHeuristic.java
@@ -1,6 +1,4 @@
-import java.util.List;
-
-public class PrimaryHeuristic extends Heuristic {
+public class CenterHeuristic extends Heuristic {
 
     private static final int SCORE_WEIGHT = 1000;
 
@@ -9,9 +7,7 @@ public class PrimaryHeuristic extends Heuristic {
         if (Minimax.gameOver(state))
             return winLoss(state);
 
-        List<OrderedMove> minMoves = MoveGenerator.generate(state.board, state.minimizingPlayer, state.maximizingPlayer);
         return closenessToCenter(state.board, state.maximizingPlayer)
-                - aggressionFactor(minMoves)
                 + state.maxPlayerScore * SCORE_WEIGHT - state.minPlayerScore * SCORE_WEIGHT;
     }
 }
