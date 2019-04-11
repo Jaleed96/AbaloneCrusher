3ba33ef1baae5ef47ba8ecbd2a8c72498c262f21
Jaleed Ur Rehman <jaleed12@me.com>
2019-04-11 09:04:33 -0700

    HAIL MARYYYYYYY!!!!

--- src/PrimaryHeuristic.java
+++ src/PrimaryHeuristic.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class PrimaryHeuristic extends Heuristic {
 
     private static final int SCORE_WEIGHT = 1000;
+    private static final int CENTER_WEIGHT = 2;
 
     @Override
     public int evaluate(final Minimax.State state) {
@@ -10,8 +11,9 @@ public class PrimaryHeuristic extends Heuristic {
             return winLoss(state);
 
         List<OrderedMove> minMoves = MoveGenerator.generate(state.board, state.minimizingPlayer, state.maximizingPlayer);
-        return closenessToCenter(state.board, state.maximizingPlayer)
+        return closenessToCenter(state.board, state.maximizingPlayer) * CENTER_WEIGHT
                 - aggressionFactor(minMoves)
+                - grouping(state.board, state.minimizingPlayer)
                 + state.maxPlayerScore * SCORE_WEIGHT - state.minPlayerScore * SCORE_WEIGHT;
     }
 }
